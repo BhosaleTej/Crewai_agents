74835464bee3de0cfd2940779a048583eb668383
BhosaleTej <tejasbhosale7875@gmail.com>
2025-11-02 19:14:49 +0530

    adding test file

---
+++ test.py
@@ -0,0 +1,13 @@
+
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+ngrok_url = os.getenv("NGROK_URL")
+
+app = FastAPI()
+
+@app.get("/info")
+def get_info():
+    return {"public_url": ngrok_url}
