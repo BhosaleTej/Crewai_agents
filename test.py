3fda98bb5bdaed77999b9d808c6bf7013bc96bfa
BhosaleTej <tejasbhosale7875@gmail.com>
2025-11-04 22:59:32 +0530

    addition of function

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def get_info():
 # for third testing of webhook trigger after stash
 
 #addition function
-def adad(a,b):
+def adada(a,b):
     'addiation of two num'
-    c = a * b
+    c = a * b * c
     return c
